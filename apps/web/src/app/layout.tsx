ca3a5db737fa4c5278d55d99954b896264a74bc1
Eric Sousa <ericrafaelsousamorel@gmail.com>
2026-05-05 00:08:03 -0400

    Add Vercel Web Analytics

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/next';
 import AmbientMusicPlayer from '../components/AmbientMusicPlayer';
 import SMChatbot from '../components/SMChatbot';
 
@@ -31,6 +32,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         {children}
+        <Analytics />
         <SMChatbot />
         <AmbientMusicPlayer />
         
